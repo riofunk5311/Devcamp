b87d0c6a564747ad7383ede582eabe048884744d
Ryota Sugawara <r_sugawara@rossolabel.com>
2018-07-05 13:55:25 +0900

    Implemented image defaults for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,4 +6,11 @@ class Portfolio < ApplicationRecord
   end
 
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: "Ruby on Rails") }
+
+  after_initialize :set_deafults
+
+  def set_deafults
+    self.main_image ||= "http://via.placeholder.com/600x400"
+    self.thumb_image ||= "http://via.placeholder.com/350x200"
+  end
 end
